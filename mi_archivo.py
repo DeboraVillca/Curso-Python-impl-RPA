a13171bb6e175024f087e03912ab55a60a588af1
DeboraVillca <debyvillca95@gmail.com>
2021-12-05 23:45:16 -0300

    class one and two

--- mi_archivo.py
+++ mi_archivo.py
@@ -1,3 +1,4 @@
+#pruebas de las clases 1 y 2 
 numeros=[1, 2, 3, 4, 5]
 
 numeros_dobles=[]
@@ -6,3 +7,15 @@ for numero in numeros:
     numeros_dobles.append(numero*2)
 
 print(numeros_dobles)
+
+#with open(file) as var:
+#procesar el archivo
+#reverse, copia la lista y la da vuelta(menos trabajoso que copiar y darla vuelta)
+numeros_reverse=numeros[::-1]
+print(numeros_reverse)
+#no es bueno tener dos listas para muchos elementos
+print(numeros[:])#imprime todos los elementos
+#tuplas similar a listas , inmutable,metodos count()index()
+#todo pude ser anidado ,para el tamaño es print(len(tupla))
+#no puede ser compresion list
+
